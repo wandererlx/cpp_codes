8197eea09a4b53450a89fb0b4891ac722a825d65
wandererlx <lixiaouni@qq.com>
2024-07-23 22:00:50 +0800

    fix

--- cpp_test/float_num/main.cc
+++ cpp_test/float_num/main.cc
@@ -8,9 +8,12 @@
 
 int main(int argc, char* argv[]) {
   double data = 8.11;
+  printf("%f\n", data);
   printf("%.2f\n", data);
   printf("%.16f\n", data);
-  std::cout << std::setprecision(19) << data << std::endl;
+  // 默认输出精度是6，不显示后边的0
+  std::cout << "std::cout:" << data << std::endl;
+  std::cout << "std::cout with precision 19:" << std::setprecision(19) << data << std::endl;
   double data2 = 3.14159;
   printf("data2:%.16f\n", data2);
   printf("3 width data2:%.3f\n", data2);
